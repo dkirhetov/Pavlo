15d13850a2c2e125beccadb672894e5c26bfa550
Daniel Kirhetov <daniell2813@gmail.com>
2021-06-29 00:43:44 +0300

    Added comments

--- Clicker Pavlo 2.0.py
+++ Clicker Pavlo 2.0.py
@@ -3,10 +3,10 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys 
 import os
 
-top = 30
+top = 30                                         #starting coordinates of labels/buttons
 
 
-os.system("mode con cols=25 lines=4")
+os.system("mode con cols=25 lines=4")            #Andrey, add something, I don't know what is it
 
 
 class Pavlo(QMainWindow):
@@ -15,27 +15,28 @@ class Pavlo(QMainWindow):
         super(Pavlo,self).__init__()
 
         self.setWindowTitle("Pavlo")
-        self.setGeometry(80,38,500,500)
+        self.setGeometry(80,38,500,500)         #initializing the window
 
         
 
-        self.button = QtWidgets.QPushButton(self)
+        self.button = QtWidgets.QPushButton(self)       #adding button
         self.button.setText("Add match")
         self.button.adjustSize()
-        self.button.clicked.connect(self.addMatch)
+        self.button.clicked.connect(self.addMatch)      #button reacting
 
-    def addMatch(self):
+    def addMatch(self):                                 #button reaction
         global top
-        self.text1 = QtWidgets.QPushButton(self)
+        self.text1 = QtWidgets.QLabel(self)             #creating text field
         self.text1.setText("hello")
         self.text1.move(10,top)
         self.text1.adjustSize()
-        self.text1.show()
-        top += 20
+        self.text1.show()                               #make it visible
+        top += 20                                       #move starter coordinates down
 
  
         
-
+        #below some shit is happening
+        #only Andrey knows it
 
  
 def Clicker():
@@ -46,5 +47,4 @@ def Clicker():
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
-
     Clicker()
