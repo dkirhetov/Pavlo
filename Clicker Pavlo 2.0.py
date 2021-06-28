665696285e1483d4bc8ba038c6cb8d6ec5946b8d
Daniel Kirhetov <daniell2813@gmail.com>
2021-06-29 00:16:20 +0300

    qq

--- Clicker Pavlo 2.0.py
+++ Clicker Pavlo 2.0.py
@@ -3,18 +3,19 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys 
 import os
 
+top = 30
+
+
 os.system("mode con cols=25 lines=4")
-global top
-top = 10
+
 
 class Pavlo(QMainWindow):
 
     def __init__(self):
         super(Pavlo,self).__init__()
 
-
         self.setWindowTitle("Pavlo")
-        self.setGeometry(0,0,500,500)
+        self.setGeometry(80,38,500,500)
 
         self.text1 = QtWidgets.QLabel(self)
 
@@ -22,27 +23,29 @@ class Pavlo(QMainWindow):
         self.button.setText("Add match")
         self.button.adjustSize()
         self.button.clicked.connect(self.addMatch)
-
-
         
-
 
     def addMatch(self):
+        global top
         self.text1.setText("hello")
-        self.text1.move(top,40)
+        self.text1.move(10,top)
         self.text1.adjustSize()
-        top +=10
+        top += 20
 
- 
+
         
 
 
 
+
+ 
 def Clicker():
+
     app = QApplication(sys.argv)
     window = Pavlo()
     window.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
+
     Clicker()
